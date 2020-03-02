6c0291e2d9cb317fc4feb18db9979810f8adb67e
Caleb DeLaBruere <delabrcd@clarkson.edu>
2020-03-02 11:24:47 -0500

    fix: fixed sidebar and appbar locations

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,32 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./body.css";
-import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Sidebar from "./Sidebar";
 import { Layout } from "antd";
 import cubejs from "@cubejs-client/core";
 import { CubeProvider } from "@cubejs-client/react";
 import WebSocketTransport from "@cubejs-client/ws-transport";
-import clsx from 'clsx';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from './components/listitems';
 import { useStyles } from './Styles';
+
+
 
 const drawerWidth = 240;
 
@@ -55,68 +40,24 @@ function Copyright() {
   );
 }
 
-const AppLayout = ({ children }) => {
-
+const App = ({ children }) => 
+{
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-  return (
-    <Layout style={{height:"100%"}}>
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            TAC Turbine Dashboard
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>{mainListItems}</List>
-      </Drawer>
-      <main>
-        <Layout.Content>{children}</Layout.Content>
-      </main>
+return (
+  <div className={classes.root}>
+  <CssBaseline/>
+  <Sidebar/>
+    <main className={classes.Content}>
+      <div className={classes.appBarSpacer}>
+        <CubeProvider cubejsApi={cubejsApi}>
+          <Layout.Content>{children}</Layout.Content>
+        </CubeProvider>
       </div>
-    </Layout>
-  );
-};
-
-const App = ({ children }) => (
-  <div>
-  <CubeProvider cubejsApi={cubejsApi}>
-    <AppLayout>{children}</AppLayout>
-  </CubeProvider>
+    </main>
   <Copyright/>
   </div>
 
 );
-
+}
 export default App;
--- dashboard-app/src/index.js
+++ dashboard-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
 import * as serviceWorker from "./serviceWorker";
 import { HashRouter as Router, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
