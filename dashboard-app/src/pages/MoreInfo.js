467ab7b2c2f15f79d8e171951a519910cc77e304
Caleb DeLaBruere <delabrcd@clarkson.edu>
2020-04-02 20:24:23 +0000

    feat: updated page switching

---
+++ dashboard-app/src/pages/MoreInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import ChartRenderer from "../components/ChartRenderer";
+import Dashboard from "../components/Dashboard";
+import DashboardItem from "../components/DashboardItem";
+import { useStyles } from "../Styles"
+
+
+const MoreInfo = () => {
+    const classes = useStyles()
+    return (
+        <div className={classes.container}>
+            <h1>more info</h1>
+        </div>
+    )
+};
+
+
+export default MoreInfo;
