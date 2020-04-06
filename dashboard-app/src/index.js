0613bf7c003def63c6221f960ad3d9f043c32c70
Caleb DeLaBruere <delabrcd@clarkson.edu>
2020-04-06 15:45:12 -0400

    feat: home page formatting and added live view

--- dashboard-app/src/index.js
+++ dashboard-app/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Sidebar from "./Sidebar";
 import App from "./App";
-
+import { useStyles } from "./Styles"
 import * as serviceWorker from "./serviceWorker";
 import { HashRouter as Router, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
@@ -10,17 +12,31 @@ import HistoricalData from "./pages/HistoricalData";
 import SystemPage from "./pages/System";
 import MoreInfo from "./pages/MoreInfo";
 
+function Test() {
+  const classes = useStyles();
+  return (
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Sidebar />
+        <div className={classes.content}>
+          <App>
+            <Route exact path="/" component={DashboardPage} />
+            <Route exact path="/historical_data" component={HistoricalData} />
+            <Route exact path="/system" component={SystemPage} />
+            <Route exact path="/more_info" component={MoreInfo} />
+          </App>
+        </div>
+      </div>
+    </Router>
+  );
+
+};
+
 
 ReactDOM.render(
-  <Router>
-    <App>
-      <Route exact path="/" component={DashboardPage} />
-      <Route exact path="/historical_data" component={HistoricalData} />
-      <Route exact path="/system" component={SystemPage} />
-      <Route exact path="/more_info" component={MoreInfo} />
-
-    </App>
-  </Router>,
+  <Test />
+  ,
   document.getElementById("root")
 ); // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
