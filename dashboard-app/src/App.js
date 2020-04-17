52025d1ca160f31a10167f58466bd8ec82583364
Caleb DeLaBruere <delabrcd@clarkson.edu>
2020-04-16 21:52:50 -0400

    oopy

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -8,17 +8,8 @@ import { useStyles } from './Styles';
 import Container from '@material-ui/core/Container';
 
 export const API_URL = "http://localhost:4000";
-const CUBEJS_TOKEN =
+export const CUBEJS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODU1Nzk4Mjl9.0aSyAcNtMg2zU6M6ufshAr2-SBrZPvoC9_ljvdkvhNY";
-const cubejsApi = cubejs({
-  transport: new WebSocketTransport({
-    authorization: CUBEJS_TOKEN,
-    apiUrl: API_URL.replace("http", "ws")
-  })
-});
-
-
-
 
 function Copyright() {
   return (
