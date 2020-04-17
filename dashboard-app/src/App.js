35e89d18450a4f93a3512afd8c135bd37cb400af
Zachary Damato <zacharyd1@live.com>
2020-04-16 22:05:12 -0400

    Revert "oopy"
    This reverts commit 52025d1ca160f31a10167f58466bd8ec82583364.

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -8,8 +8,17 @@ import { useStyles } from './Styles';
 import Container from '@material-ui/core/Container';
 
 export const API_URL = "http://localhost:4000";
-export const CUBEJS_TOKEN =
+const CUBEJS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODU1Nzk4Mjl9.0aSyAcNtMg2zU6M6ufshAr2-SBrZPvoC9_ljvdkvhNY";
+const cubejsApi = cubejs({
+  transport: new WebSocketTransport({
+    authorization: CUBEJS_TOKEN,
+    apiUrl: API_URL.replace("http", "ws")
+  })
+});
+
+
+
 
 function Copyright() {
   return (
